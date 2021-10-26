da0e381cd9d63095ff38a9f328c245fe4a06e229
JancyCapellan <47362837+JancyCapellan@users.noreply.github.com>
2021-10-25 22:03:57 -0400

    *cp* refactoring files to be more concise

---
+++ courier/components/Customers/main/CustomerTable.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useCart } from '../../../contexts/cartContext'
+import { useRouter } from 'next/router'
+const router = useRouter()
+
+function CustomerTable({ search, setCurrentUser, setShowEditor }) {
+  //search is done with or statements in mysql wiht form entry search bar component,
+  // might add component into this file
+  const [searchResults, setSearchResults] = useState([])
+  const { changeCurrentOrderUser } = useCart()
+
+  // TODO: api/services/app/Customer/GetAll?Filter=m&SkipCount=0&MaxResultCount=10
+
+  useEffect(() => {
+    async function getCustomers() {
+      try {
+        let res = await axios.get(`http://localhost:3000/user/getUsers?search='${search}'`)
+        if (res.status === 200) {
+          // console.log('herere', res.data)
+          setSearchResults(res.data)
+        }
+      } catch (error) {
+        console.log('getcustomer', error)
+      }
+    }
+    getCustomers(search)
+    // console.log(searchResults)
+  }, [search, setSearchResults])
+
+  function openCustomerAccountPage(user) {
+    router.push()
+    const location = {
+      pathname: '/customers/customeraccount',
+      state: { user: user },
+    }
+  }
+  return (
+    <>
+      {/* <div className='customer-manager-page'>
+        <div className='customer-search'>
+          <label>
+            <input placeholder='search customers' onChange={handleChange}></input>
+          </label>
+          <button className='customer-search-btn' onClick={console.log(search)}>
+            search
+          </button>
+        </div>
+      </div> */}
+
+      <section className='customers-table'>
+        <h3>Customers</h3>
+        <table>
+          <tbody>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+            </tr>
+            {searchResults.map((item) => {
+              return (
+                <tr className='customer-table-row' key={item.id}>
+                  <td onClick={() => setCurrentUser(item)}>{item.id}</td>
+                  <td onClick={() => openCustomerAccountPage(item)}>
+                    {item.first_name} {item.middle_name} {item.last_name}
+                  </td>
+                  {/* <td>
+                    <button
+                      onClick={() => {
+                        setCurrentUser(item)
+                        setShowEditor(true)
+                      }}
+                    >
+                      edit
+                    </button>
+                  </td> */}
+                  <td>
+                    <button
+                      onClick={() => {
+                        console.log(`move to order page with ${item.first_name} info`)
+                        setCurrentUser(item)
+                        changeCurrentOrderUser(item)
+                        history.push('/createcustomerorder')
+                      }}
+                    >
+                      order
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+    </>
+  )
+}
+
+export default CustomerTable
---
+++ courier/pages/customers/index.tsx
@@ -0,0 +1,7 @@
+import CustomerManager from '../../components/Customers/main/CustomerManager'
+
+const customers = () => {
+  return <CustomerManager />
+}
+
+export default customers
