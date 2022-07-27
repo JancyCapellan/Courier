f9a21177ee16fca3d1e35d02e30653a3f798c847
JancyCapellan <jancycapellan97@gmail.com>
2022-07-27 17:15:54 -0400

    dockerized and started devops pipeline

--- courier/next.config.js
+++ courier/next.config.js
@@ -2,3 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 module.exports = withBundleAnalyzer({})
+module.exports = {
+  reactStrictMode: true,
+  output: 'standalone',
+}
