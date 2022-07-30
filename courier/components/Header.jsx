44a59779652d52351a13e8a0730833096b7c493c
Jancy Capellan <jancycapellan97@gmail.com>
2022-07-30 15:24:55 -0400

    nginx or dokcer not conencting to API/ cors errors

--- courier/components/Header.jsx
+++ courier/components/Header.jsx
@@ -10,7 +10,9 @@ function Header() {
         <span>
           Welcome {session?.user.name}! <b>Role: {session?.user.role}</b>
         </span>
-        <button onClick={() => signOut({ callbackUrl: process.env.NEXTAUTH_URL })}>Sign out</button>
+        <button onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_API_URL })}>
+          Sign out
+        </button>
       </section>
     </header>
   )
