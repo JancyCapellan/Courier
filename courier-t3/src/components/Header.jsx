c801c40107c94b09bee8533755d88570cde3ff0f
jancycapellan <jancycapellan97@gmail.com>
2022-09-19 14:50:51 -0400

    fixed overflow

--- courier-t3/src/components/Header.jsx
+++ courier-t3/src/components/Header.jsx
@@ -7,8 +7,9 @@ function Header() {
 
   // console.log('header session data', session, 'status', status)
   return (
-    <header className='header flex flex-col justify-center bg-black w-screen h-12 py-1 px-2'>
+    <header className='header flex flex-col justify-center bg-black h-12 py-1 px-2'>
       <section className='flex flex-row justify-between text-white'>
+        <div>Courier's Dashboard</div>
         <div>
           Welcome {session?.user.name}! |{' '}
           <b className='text-red-600'>Role: {session?.user.role}</b>
