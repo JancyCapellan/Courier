d76412a0e866c3674476976c734f7f40067b2dd3
jancycapellan <jancycapellan97@gmail.com>
2022-09-26 12:58:11 -0400

    added ESC key event to close modals

--- courier-t3/src/components/HOC/ModalContainer.jsx
+++ courier-t3/src/components/HOC/ModalContainer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 //show is the state of the display t/f
 //handleclose is the toggle/close fn for the modal
 const ModalContainer = ({ show, handleClose, children }) => {
   const showHideClassName = show ? 'd-block' : 'd-none'
+
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (show) {
+        // console.log('User pressed: ', event.key)
+
+        if (event.key === 'Escape') {
+          event.preventDefault()
+
+          // 👇️ your logic here
+          handleClose()
+        }
+      }
+    }
+
+    document.addEventListener('keydown', keyDownHandler)
+
+    // 👇️ clean up event listener
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler)
+    }
+  }, [show])
   return (
     show && (
       <div
