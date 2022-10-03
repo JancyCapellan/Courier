c941f77de50cbf77a1668905f99dafbdb6046520
jancycapellan <jancycapellan97@gmail.com>
2022-10-03 09:11:44 -0400

    persist didnt work the way i thought, changing to database.

---
+++ courier-t3/src/pages/createOrder/text.jsx
@@ -0,0 +1,12 @@
+import Cart from '@/components/pages/order/CreateOrder/Cart'
+import React from 'react'
+
+const text = () => {
+  return (
+    <div>
+      <Cart />
+    </div>
+  )
+}
+
+export default text
