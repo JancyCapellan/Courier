430692fbef345ed4056ef5c608a7c7aab31075e2
Jancy Capellan <jancycapellan97@gmail.com>
2023-02-27 16:45:00 -0500

    debugged route guard, updated JWT typing

--- courier-t3/src/middleware.ts
+++ courier-t3/src/middleware.ts
@@ -1,23 +1,22 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware'
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
+  // only runs if the authroized callback is true
   function middleware(req) {
-    console.log('NEXTUATH middleware token:', req.nextauth.token)
+    console.log('MIDDLEWARE NEXTAUTH RAN:', req.nextauth.token)
   },
   {
     callbacks: {
       authorized({ req, token }) {
         // `/admin` requires admin role
-        if (req.nextUrl.pathname === "/administration") {
-          return token?.role === "admin"
-        }
-        // `/me` only requires the user to be logged in
-        return !!token
+        if (req.nextUrl.pathname === '/administration') {
+          //console.log('auth token:', token?.role, !token) //?  is token missing role becuase of its typing?
+          return token?.user?.role === 'ADMIN'
+        } else return false // this way i can do more than just rely on the matcher to match the url.
       },
     },
-
   }
 )
 
-export const config = { matcher: ["/administration"] }
+export const config = { matcher: ['/administration'] }
--- courier-t3/src/types/next-auth.d.ts
+++ courier-t3/src/types/next-auth.d.ts
@@ -1,13 +1,26 @@
 import { DefaultSession } from 'next-auth'
-// import { JWT } from 'next-auth/jwt'
+import { JWT } from 'next-auth/jwt'
 
-// declare module 'next-auth/jwt' {
-//   /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
-//   interface JWT {
-//     /** OpenID ID Token */
-//     idToken?: string
-//   }
+// TODO: correctly type jwt for app needs
+//  interface DefaultJWT extends Record<string, unknown> {
+//     name?: string | null;
+//     email?: string | null;
+//     picture?: string | null;
+//     sub?: string;
 // }
+// export interface JWT extends Record<string, unknown>, DefaultJWT {}
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    idToken?: string
+    user?: {
+      // id: string
+      role: 'ADMIN' | 'STAFF' | 'CUSTOMER'
+    }
+  }
+}
 
 declare module 'next-auth' {
   enum userRoles {
