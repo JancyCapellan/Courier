ff3f1f9ed9cf812d9b403099e7235fce960e70c9
Jancy Capellan <jancycapellan97@gmail.com>
2023-03-07 23:53:35 -0500

    error handling

--- courier-t3/src/server/stripe/stripe-webhook-handlers.ts
+++ courier-t3/src/server/stripe/stripe-webhook-handlers.ts
@@ -3,12 +3,13 @@ import type Stripe from 'stripe'
 import { getLogger } from '../../../logging/log-utils'
 import { prisma } from 'server/db/client'
 import { stripe } from './client'
+import Checkout from 'pages/createOrder/checkout/[customerId]'
 
 const logger = getLogger('Stripe-Webhooks')
 
 export const handleCheckoutSessionCompleted = async (event: Stripe.Event) => {
   const checkoutSession = event.data.object as Stripe.Checkout.Session
-  logger.info(checkoutSession)
+  // logger.info(checkoutSession)
 
   //TODO: add stripe object and cart session for the correct customer Order
   const updatedOrderWithStripeCheckout = await prisma.order.update({
@@ -33,4 +34,20 @@ export const handleCheckoutSessionCompleted = async (event: Stripe.Event) => {
     '🚀 ~ file: webhook.ts ~ line 59 ~ webhook ~ updatedOrderWithStripeCheckout',
     updatedOrderWithStripeCheckout
   )
+}
+
+export const handleCheckoutSessionExpired = async (event: Stripe.Event) => {
+  const expiredCheckoutSession = event.data.object as Stripe.Checkout.Session
+
+  try {
+    const removedCheckoutSession = await prisma.order.delete({
+      where: {
+        stripeCheckoutId: expiredCheckoutSession.id,
+      },
+    })
+
+    // logger.info(removedCheckoutSession)
+  } catch (err) {
+    logger.error(err)
+  }
 }
