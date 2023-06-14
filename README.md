fcb01ffd6abc5b07261db1b28a8c7faf76dcebd6
Jancy Capellan <jancycapellan97@gmail.com>
2023-06-14 10:08:49 -0400

    creating landing page

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ other courier folders will probably have clones of the original project in a dif
 one will be nextjs fullstack with trpc, probably look into using the create-t3 app.
 
 dont use npm install in courier folder, package.json is redundant to package.json in the outer folder of courier.
+
+# README
+
+In Development, web native app that runs also on mobile. this applications purpose is to modernize the courier type shipping companies that are popular in latin america and asia. the current clients move a lot of boxes ( of a mostly uniform size), futurnite and other misc items for varying costs. every orders has packages that must be tracked with labels with specific requirements, and all of this was tracked for a long time on paper and manually with excel. this modern system gives this companies and future clients in the industry to manage all aspects of their business, staff, customers and orders in one location and with the ability to manange all their data in a database.
+
+# Project Synopsis
+
+the front end of the application is written in next.js and with the create-t3-app but was started as an cold fusion backend (very long story) then progressed to a custom react stack finally ending on nextjs. the version in the Courier Folder, is basically written from scratch with raw css, sql, custom tables. but then i decided that a rewrite was needed/
--- courier-t3/src/pages/index.tsx
+++ courier-t3/src/pages/index.tsx
@@ -30,108 +30,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex flex-row w-full h-16 bg-gray-500 items-center mx-auto just">
+      <div className="flex flex-row w-full h-16 bg-black text-white items-center mx-auto">
         The Courier Dashboard App
+        <button
+          className="btn btn-blue"
+          onClick={() => {
+            router.push('/login')
+          }}
+        >
+          login
+        </button>
       </div>
-      <div className="w-screen flex flex-col items-center grow bg-cyan-600">
-        {router.query?.didRegister}
-        {router.query?.error ? <p>{router.query.error}</p> : <></>}
-        {error ? (
-          <p>
-            THERE WAS AN ERROR WHILE LOGGING. CHECK TO MAKE SURE THAT YOU ARE
-            USING THE CORRECT USERNAME AND PASSWORD.
-          </p>
-        ) : (
-          <></>
-        )}
-        {!!session ? (
-          <div className="flex flex-col w-fit justify-center mt-16">
-            <p> Logged in as {session?.user?.name} </p>
-            <button
-              className="btn btn-blue"
-              onClick={() =>
-                signOut({ callbackUrl: process.env.NEXT_PUBLIC_URL_API })
-              }
-            >
-              Sign out
-            </button>
-            <Link href="/account" passHref>
-              <button className="btn btn-blue">account</button>
-            </Link>
-          </div>
-        ) : (
-          <Formik
-            // initialValues={{ email: '', password: '', tenantKey: '' }}
-            initialValues={{ email: '', password: '' }}
-            validationSchema={Yup.object({
-              email: Yup.string()
-                .max(30, 'Must be 30 characters or less')
-                .email('Invalid email address')
-                .required('Please enter your email'),
-              password: Yup.string()
-                .required('Please enter your password')
-                .min(3, 'password is too short'),
-              // tenantKey: Yup.string()
-              //   .max(20, 'Must be 20 characters or less')
-              //   .required('Please enter your organization name'),
-            })}
-            onSubmit={async (values, { setSubmitting }) => {
-              const res = await signIn('login', {
-                redirect: false,
-                email: values.email,
-                password: values.password,
-                // tenantKey: values.tenantKey,
-                callbackUrl: process.env.NEXT_PUBLIC_API_URL + 'account',
-              })
-              // console.log('res', res)
-              if (res?.error) {
-                setError(true)
-              }
-              if (res?.url) router.push(res.url)
-              // console.log('error', error)
-              setSubmitting(false)
-            }}
-          >
-            {(formik) => {
-              return (
-                <>
-                  <Form className="flex flex-col md:w-1/2 my-24 max-w-sm">
-                    <h2 className="block w-full text-center text-grey-darkest mb-6 bold underline text-xl">
-                      Sign In
-                    </h2>
-                    <FormikControl
-                      control="input"
-                      type="email"
-                      label="Email"
-                      name="email"
-                    />
-                    <FormikControl
-                      control="input"
-                      type="password"
-                      label="Password"
-                      name="password"
-                    />
-                    <div className="flex flex-col items-center">
-                      <button
-                        type="submit"
-                        disabled={!formik.isValid}
-                        className="btn btn-blue m-2 w-1/2 align-center"
-                      >
-                        Login
-                      </button>
-                      <Link href="/register" passHref>
-                        <button className="btn btn-blue m-2 w-1/2">
-                          Don{`&apos`}t have an account?{' '}
-                          <span className="text-red-400">Register here.</span>
-                        </button>
-                      </Link>
-                    </div>
-                  </Form>
-                </>
-              )
-            }}
-          </Formik>
-        )}
+      <div className="w-screen flex flex-col items-center grow">
+        <p className="align-middle">
+          Courier Dashbaord is an application to handle all the needs for
+          international shipping for specifically the caribean but can be
+          expanded to different regions. The goal of this app was to make a
+          business management app that would allow buisnesses to have a better
+          online presence and have a more modern control of their buisness data.
+          customers would also be able to use the application to make and check
+          on their orders and account information.
+        </p>
       </div>
     </div>
   )
